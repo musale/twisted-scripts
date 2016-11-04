c72112b548e1e52dd0f3fe0f32ccd04d7ca71afd
musale <musale@focusmobile.com>
2016-11-04 16:47:31 +0300

    [initial commit]:: working on deferreds

---
+++ callback.py
@@ -0,0 +1,29 @@
+from twisted.internet.defer import Deferred
+
+
+def callbacker(result):
+    print result
+
+
+def multiple_callbacks(d):
+    d.addCallback(callback_a)
+    d.addCallback(callback_b)
+    d.addCallback(print_html)
+    d.callback("slay!!")
+
+
+def callback_a(result):
+    return '<b>%s</b>' % (result,)
+
+
+def callback_b(result):
+    return '<i>%s</i>' % (result,)
+
+
+def print_html(result):
+    print result
+
+
+if __name__ == 'main':
+    d = Deferred()
+    multiple_callbacks(d)
